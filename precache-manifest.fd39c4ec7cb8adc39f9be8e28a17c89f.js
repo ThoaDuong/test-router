018865d8da5577d1dfa83b71150c1a652c57cc0e
Thoa Duong <kimthoa2598@gmail.com>
2020-04-18 19:06:40 +0700

    Updates

---
+++ precache-manifest.fd39c4ec7cb8adc39f9be8e28a17c89f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c730d89148c555e7ac49176b9b942b20",
+    "url": "/test-router/index.html"
+  },
+  {
+    "revision": "97c482cb337681b07446",
+    "url": "/test-router/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fb5ba14ee064a8ec8f0f",
+    "url": "/test-router/static/js/2.05f4efdc.chunk.js"
+  },
+  {
+    "revision": "97c482cb337681b07446",
+    "url": "/test-router/static/js/main.5519ffff.chunk.js"
+  },
+  {
+    "revision": "81b61122865c6eb192b9",
+    "url": "/test-router/static/js/runtime-main.5606bd89.js"
+  }
+]);
